620c1c190c8bd9257970dec65a6bacfb02bbfb5f
simpleAdam <34377387+simpleAdam@users.noreply.github.com>
2021-02-16 23:03:48 +0000

    Create stack.test.js

---
+++ stack.test.js
@@ -0,0 +1,11 @@
+let pile = new Stack
+pile.add('treasure hoard')
+pile.add('pieces-o-eight')
+pile.add('dirt')
+pile.add('dirt')
+pile.add('dirt')
+console.log(pile.list)
+pile.pop()
+pile.pop()
+pile.pop()
+console.log(pile.list)
