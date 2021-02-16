52774fa782281e8253302d29429c9556e6665404
simpleAdam <34377387+simpleAdam@users.noreply.github.com>
2021-02-16 23:03:09 +0000

    Update stack.js

--- stack.js
+++ stack.js
@@ -1,7 +1,7 @@
 class Stack{
   constructor() {
     this.list=[];
-}
+  }
   add(item) {
     this.list.push(item)
   }
@@ -9,16 +9,3 @@ class Stack{
   this.list.pop(item)
   }
 }
-
-let pile = new Stack
-pile.add('treasure hoard')
-pile.add('pieces-o-eight')
-pile.add('dirt')
-pile.add('dirt')
-pile.add('dirt')
-console.log(pile.list)
-pile.pop()
-pile.pop()
-pile.pop()
-console.log(pile.list)
-//module.exports=Stack
